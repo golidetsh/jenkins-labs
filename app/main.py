80d41f2c273af9213421df2ebe42cea859dffa4d
eddie <edtshuma@gmail.com>
2023-02-15 09:36:38 +0100

    post request using  dictionary

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.params import Body 
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello WOrld"}
+    return {"message": "Welcome to Facebook API"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new post": f"title: {payload['title']} content:{payload['content']}"}
