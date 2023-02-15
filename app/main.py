0d4fd851227c3d8a1613d7aa724df57eca86e878
eddie <edtshuma@gmail.com>
2023-02-15 09:53:07 +0100

    add optional parameter

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.params import Body 
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -8,6 +9,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
