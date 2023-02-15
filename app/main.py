b797bce306e7ff22795e949c83af82c4551709fe
eddie <edtshuma@gmail.com>
2023-02-15 12:12:43 +0100

    add get post by ID method

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from fastapi.params import Body 
+from fastapi.params import Body
+from fastapi import FastAPI , Response, status, HTTPException 
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
@@ -31,3 +32,18 @@ def create_posts(post: Post):
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+         return p
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail =f"Post with id {id} was not found")
+    return {"Post detail" : post }
