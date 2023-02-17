e1c32bd80697f4b84e7511ff0b726a40452f5509
eddie <edtshuma@gmail.com>
2023-02-17 10:55:35 +0100

    add test for invalid post ID

--- tests/test_root.py
+++ tests/test_root.py
@@ -10,3 +10,9 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to Facebook API"}
+    
+
+def test_invalid_id():
+    response = client.get("/posts/99")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'id not found'}
