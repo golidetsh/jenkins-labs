3a1d4e4134f7cfdc0d24ba9e3d55d51618827204
eddie <edtshuma@gmail.com>
2023-02-23 19:09:12 +0100

    add helper module

---
+++ app/helpers/helper.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+courses = [
+    { id: 1, name: "Civil Engineering"},
+    { id: 2, name: "Electrical Engineering"},
+    { id: 3, name: "Software Engineering"},
+]
+
+
+module.exports.courses =  courses;
