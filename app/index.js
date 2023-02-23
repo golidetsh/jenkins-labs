dcafd8114ec5832852ca7a66858c8023c19a20f3
eddie <edtshuma@gmail.com>
2023-02-23 19:01:08 +0100

    add logging

--- app/index.js
+++ app/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 const home = require('./routes/home')
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 app.use(home);
+app.use(logger);
 
+
+if (app.get('env')  === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled ...');
+  }
+
+  
 app.listen(3000, () => console.log ('Listening on port 3000 ...'));
