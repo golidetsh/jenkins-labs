8ae496c80933400da17d8c937514705750b95a1f
eddie <edtshuma@gmail.com>
2023-02-23 19:47:25 +0100

    add get post by ID method

--- app/helpers/helper.js
+++ app/helpers/helper.js
@@ -1,10 +1,20 @@
 const express = require('express');
 
 posts = [
-    { title: 1, content: "Top 10 AI tools", published: "true", rating: 1},
-    { title: 2, content: "NODEJS in action",published: "true",rating: 4 },
-    { title: 3, content: "The making of React the documentary", published: "true",rating: 5}
+    { id: 1, title: "AI",content: "Top 10 AI tools", published: "true", rating: 1},
+    { id: 2, title: "NodeJS", content: "NODEJS in action",published: "true",rating: 4 },
+    { id: 3, title: "React", content: "The making of React the documentary", published: "true",rating: 5}
 ]
 
+function findPost(id) {
+    var arr = posts;
+    const found = arr.find(c => c.id === id);
+    if (found); 
+    return found;
+  };
 
 module.exports.posts =  posts;
+
+module.exports = {
+    findPost : findPost
+ };
--- app/routes/facebook.js
+++ app/routes/facebook.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { posts } = require('../helpers/helper');
+const { posts, findPost } = require('../helpers/helper');
 const router = express();
 
 
@@ -8,6 +8,14 @@ router.get('/api/posts', (req, res) => {
    res.send(posts);
 });
 
+
+router.get('/api/posts/:id', (req, res) => {
+     const post = findPost(parseInt(req.params.id));
+     if(!post) return res.status(404).send(`Post with id ${req.params.id} was not found`);
+  
+     res.send(post);
+   });
+  
 
 module.exports = router;
 
