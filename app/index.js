ba9eda6ada2c0c5287785099e4c4cc2a59e43c33
eddie <edtshuma@gmail.com>
2023-02-23 18:48:15 +0100

    add routing

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
+const home = require('./routes/home')
 
-app.get('/', (req, res) =>{
-    res.send('Welcome to Facebook API');
-  });
-
+app.use(home);
 
 app.listen(3000, () => console.log ('Listening on port 3000 ...'));
