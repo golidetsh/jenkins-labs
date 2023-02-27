286e4786f345256d26b776cc67b3407c613c591b
eddie <edtshuma@gmail.com>
2023-02-27 12:44:24 +0100

    update tests

--- tests/test_root.py
+++ tests/test_root.py
@@ -6,18 +6,18 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_root():
+def test_root_should_return_200():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to Facebook API"}
     
 
-def test_invalid_id():
+def test_invalid_id_should_return_404():
     response = client.get("/posts/99")
     assert response.status_code == 404
-    assert response.json() == {'detail': 'id not found'}
     
     
-def test_noninteger_id():
+    
+def test_noninteger_id_should_return_422():
     response = client.get("/posts/abc")
     assert response.status_code == 422
