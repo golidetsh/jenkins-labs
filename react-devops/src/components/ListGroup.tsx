c9d9d3851ea9119da7db8cbe4df16c60d315b917
eddie <edtshuma@gmail.com>
2023-03-31 22:56:27 +0200

    add a ListGroup

---
+++ react-devops/src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+function ListGroup() {
+
+  return (
+    <>
+      <h1>MyList</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+      </ul>
+    </>
+  );
+
+}
+
+export default ListGroup;
