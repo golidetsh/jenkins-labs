a2171b195d266f6e4919ab42e9976243960ad6ff
eddie <edtshuma@gmail.com>
2023-03-31 23:09:13 +0200

    iterate array in listgroup

--- react-devops/src/components/ListGroup.tsx
+++ react-devops/src/components/ListGroup.tsx
@@ -6,8 +6,11 @@ function ListGroup() {
     <>
       <h1>MyList</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
+        {posts.map((post) => (
+          <li className="list-group-item" key={post}>
+            {post}
+          </li>
+        ))}
       </ul>
     </>
   );
