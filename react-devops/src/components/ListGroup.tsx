94268e82b67ff171c8290e9a04356ddc1fe778f7
eddie <edtshuma@gmail.com>
2023-03-31 23:11:01 +0200

    display error when array is empty

--- react-devops/src/components/ListGroup.tsx
+++ react-devops/src/components/ListGroup.tsx
@@ -1,10 +1,12 @@
 function ListGroup() {
 
     let posts = ["Top 10 Recipes", "EPL Diaries", "Tokyo Drift Story", "Johannesburg By Night"]; 
+    posts = [];
 
   return (
     <>
       <h1>MyList</h1>
+      {posts.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {posts.map((post) => (
           <li className="list-group-item" key={post}>
