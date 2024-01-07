bc9dcd43b57c40c244ba15cf0c114ea4b287ac11
edtshuma <edtshuma@gmail.com>
2024-01-07 05:03:40 +0100

    grant iam-user access to remotestate bucket --> ACLs disabled

--- storage/main.tf
+++ storage/main.tf
@@ -10,4 +10,29 @@ data "aws_iam_user" "iam_user" {
 
 resource "aws_s3_bucket" "remote_state" {
   bucket = var.env_config[terraform.workspace].bucket_name
+
+  policy = <<EOF
+{
+   "Version":"2012-10-17",
+   "Statement":[  
+      {
+         "Effect":"Allow",
+         "Action": "s3:*",
+         "Principal": {
+            "AWS": "${data.aws_iam_user.iam_user.arn}" 
+            },
+          "Resource":"arn:aws:s3:::${var.env_config[terraform.workspace].bucket_name}/*"
+      }
+     
+   ]
+}
+EOF
+
+}
+
+resource "aws_s3_bucket_versioning" "remote_state_versioning" {
+  bucket = aws_s3_bucket.remote_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
