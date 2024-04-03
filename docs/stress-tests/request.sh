cad8108fc64ae6664b4689aab37d51bb32735760
edtshuma <edtshuma@gmail.com>
2024-04-03 15:17:12 +0200

    file cleanup

--- docs/stress-tests/request.sh
+++
@@ -1,8 +0,0 @@
-TIMES=1
-for i in $(eval echo "{1..$TIMES}")
-do
-    siege -c 1 -r 10 http://localhost:8000/docs
-    siege -c 3 -r 5 http://localhost:8000/posts/2
-    siege -c 2 -r 10 http://localhost:8000/posts
-    sleep 5
-done
